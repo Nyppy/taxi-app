9e201654abc2dbac57ba06d9487eca0de482897f
Yarosalw <yarosalw274@gmail.com>
2020-02-02 17:19:26 +0300

    1

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 var express = require('express')
-var history =  require ( ' connect-history-api-fallback ' );
-
-var app = express();
-app.use(history());
-history({
-    index: '/index.html'
-  });
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
+app.path('index.html')
+
 app.use(serveStatic(__dirname))
 var port = process.env.PORT || 5000
 app.listen(port)
