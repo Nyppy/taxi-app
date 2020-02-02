ea1826d79139720cc343401574e27b0c79b5fc87
Yarosalw <yarosalw274@gmail.com>
2020-02-02 16:47:40 +0300

    e

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
 app.use(serveStatic(__dirname))
+var middleware = history({
+    index: '/index.html'
+  });
+
 var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
